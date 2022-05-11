919ea3cb296db1998058b9629671c293a936309c
Pranesh Chowdhury EDU <91693577+Pranesh01@users.noreply.github.com>
2022-05-12 00:51:37 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// 3 functions are asta, Kaniki and Eren. 
+// Using the function. I can change the image. 
+
+function Asta() {
+    document.querySelector("#myImageId").src = "Asta.jpg";
+}
+function Kaniki() {
+    document.querySelector("#myImageId").src = "Kaniki.jpg";
+}
+function Eren() {
+    document.querySelector("#myImageId").src = "Eren.jpg";
+}
